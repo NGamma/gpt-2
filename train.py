cf765e3d37930c06b96495bdfcdcde444672b16f
NGamma <499669+NGamma@users.noreply.github.com>
2019-05-13 15:06:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from load_dataset import load_dataset, Sampler
 from accumulate import AccumulatingOptimizer
 import memory_saving_gradients
 
-CHECKPOINT_DIR = '/content/drive/My\ Drive/checkpoint/'
+CHECKPOINT_DIR = 'content/drive/My\ Drive/checkpoint'
 SAMPLE_DIR = 'samples'
 
 
